9d33a51fb45db025009b64354470fd82a14653cb
gf-man <georgefowkes94@gmail.com>
2022-11-04 19:03:23 +0000

    Forgotten but am going to the gym, nothing major

--- main.py
+++ main.py
@@ -25,8 +25,8 @@ class Exercise:
 
 def input_exercise():
     print("Add a new Exercise")
-    name = input("Exercise name:")
-    print("What type of Exercise?")
+    name = input("Exercise name: ")
+    print("What type of Exercise? ")
     category = int(input("1: Resistance, 2: Bodyweight, 3: Isometric, 4: Distance"))
     exercise_list.append(Exercise(name, category))
 
@@ -34,25 +34,25 @@ def input_exercise():
 def input_record():
     print("Record Exercise")
     print(', '.join([ex.name for ex in exercise_list]))
-    record_exercise = input("Exercise name:")
+    record_exercise = input("Exercise name: ")
     for ex in exercise_list:
         if record_exercise == ex.name:
             if ex.category == Exercise_Type["Resistance"]:
-                weight = input("Weight:\n")
-                reps = input("Reps:\n")
-                sets = input("Sets:\n")
+                weight = input("Weight(kilograms): ")
+                reps = input("Reps: ")
+                sets = input("Sets: ")
                 ex.add_record({get_date(): [weight + "kg", reps, sets]})
             elif ex.category == Exercise_Type["Bodyweight"]:
-                reps = input("Reps:\n")
-                sets = input("Sets:\n")
+                reps = input("Reps: ")
+                sets = input("Sets: ")
                 ex.add_record({get_date(): [reps, sets]})
             elif ex.category == Exercise_Type["Isometric"]:
-                time = input("Time:\n")
-                reps = input("Sets:\n")
+                time = input("Time(seconds): ")
+                reps = input("Sets: ")
                 ex.add_record({get_date(): [time + "s", reps]})
             elif ex.category == Exercise_Type["Distance"]:
-                time = input("Time:\n")
-                dist = input("Distance:\n")
+                time = input("Time(minutes): ")
+                dist = input("Distance(kilometers): ")
                 ex.add_record({get_date(): [time + "m", dist + "km"]})
 
             if get_date() not in date_list:
@@ -62,20 +62,22 @@ def input_record():
 def view_todays_records():
     for ex in exercise_list:
         if get_date() in ex.record:
-            print(ex.name, ex.record[get_date()])
+            print(ex.name, "x".join(ex.record[get_date()]))
 
 def view_exercise_records():
-    ex_name = input("Exercise name:")
+    ex_name = input("Exercise name: ")
     for ex in exercise_list:
          if ex_name == ex.name:
             for record in ex.record:
-                print(record, ex.record[record])
+                print(record, "x".join(ex.record[record]))
 
 def view_all_records():
     for rec_date in date_list:
+        print(rec_date)
         for ex in exercise_list:
             if rec_date in ex.record:
-                print(ex.name, ex.record[rec_date])
+                print(ex.name, "x".join(ex.record[rec_date]))
+        print()
 
 
 def load_records():
@@ -95,7 +97,6 @@ def load_records():
                 ex_data = line[2:].split(':')
                 ex_name = ex_data[0]
                 ex_rec = ex_data[1].split('x')
-                print(ex_name)
                 if ex_rec[0][-2:] == "kg":
                     # Resistance
                     ex_cat = 1
@@ -114,7 +115,6 @@ def load_records():
                 for ex in exercise_list:
                     if ex.name == ex_name:
                         ex.add_record({rec_date: ex_rec})
-                        print("added record", rec_date, ex_rec)
                 if rec_date not in date_list:
                     date_list.append(rec_date)
 
@@ -129,7 +129,7 @@ def save_records():
             if rec_date in ex.record:
                 output_line = "- " + ex.name + ":" + "x".join(ex.record[rec_date]) + "\n"
                 file.write(output_line)
-            file.write("\n")
+        file.write("\n")
     file.close()
     print("Records Saved")
 
@@ -137,7 +137,7 @@ if __name__ == "__main__":
     print(get_date())
     load_records()
     while True:
-        print("N: New Exercise, R: Add Record, V: View Today's Record, A: View All Records, E: View Exercise Records")
+        print("N: New Exercise, R: Add Record, V: View Today's Record, A: View All Records, E: View Exercise Records, S: Save Records")
         option = input()
         if option.lower() == "n":
             input_exercise()
